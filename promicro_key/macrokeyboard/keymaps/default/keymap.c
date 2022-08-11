1fbd0f30a3b34ee6fca3f34ec3d1d4986b13f111
bsilva <bsilva@registro.br>
2022-08-11 10:45:32 -0300

    Implementing a Special Key for a dead button function

--- promicro_key/macrokeyboard/keymaps/default/keymap.c
+++ promicro_key/macrokeyboard/keymaps/default/keymap.c
@@ -4,6 +4,22 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	KEYMAP(
-		KC_GRV, KC_F13, KC_F14),
+		0xA5, KC_F13, KC_F14),
+//       Pin9    Pin8    Pin7`
+};
 
+// 0xA5 is a Special Key (https://github.com/qmk/qmk_firmware/blob/master/docs/keycodes_basic.md#special-keys)
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case 0xA5:
+      if (record->event.pressed) {
+        SEND_STRING("`");
+      } else {
+            // when keycode QMKBEST is released
+      }
+      return false;
+      break;
+  }
+  return true;
 };
