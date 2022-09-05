ea96dce67f6c4f8e7c5cd6dedba9e57c5bff7199
bsilva <bsilva@registro.br>
2022-09-05 10:16:04 -0300

    Initial Tests

---
+++ MacroKeyboard_python/macrokeyboard.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+
+import subprocess
+import sys
+
+#arguments 0: gnome-terminal 
+# args: ['gnome-terminal', '--', 'ping', '-c', '10', '8.8.8.8']
+
+subprocess.run(
+#    ['ls', "/"]
+#    ['firefox', 'https://docs.python.org/3/library/subprocess.html']
+#    ['gnome-terminal', '--', 'ping', '-c', '10', '8.8.8.8']
+)
